83660ea6121c32b40d5a11f31230281093346cb7
gbhatiaRV <69327019+gbhatiaRV@users.noreply.github.com>
2020-09-16 11:48:36 -0500

    Add files via upload

---
+++ MenuTemplate/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+type dish struct {
+	item string
+}
+
+type menu struct {
+	dishes   []dish
+	menuType string
+}
+
+func main() {
+
+	var m []menu
+	var d, d1 []dish
+
+	d = []dish{dish{"Eggs"}, dish{"toast"}, dish{"hashBrown"}}
+	d1 = []dish{dish{"Pizza"}, dish{"Pasta"}, dish{"Salad"}, dish{"Tomato Soup"}}
+
+	m = []menu{menu{
+		dishes:   d,
+		menuType: "BreakFast",
+	},
+		menu{
+			dishes:   d1,
+			menuType: "Lunch",
+		}}
+
+	err := tpl.Execute(os.Stdout, m)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
